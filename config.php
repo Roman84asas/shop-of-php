6f772dd6a2ffbd2be395d3c4662a003f1e8aafc6
RomanMereneanu <asasin84@mail.ru>
2019-10-16 16:31:27 +0300

    init

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+defined('VG_ACCESS') or die('Access Denied');
+
+
+const SITE_URL = 'http//http://shop-of-php/';
+const PATH     = '/';
+
+const HOST     = 'localhost';
+const USER     = 'root';
+const PASSWORD = '';
+const DB_NAME  = 'shop';
+
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+define('VG_ACCESS', true);
+
+header('Content-Type: text/html; charset=utf-8');
+session_start();
+
+
+include_once 'config.php';
+include_once 'core/base/settings/settings.php';
+
+echo 'hello';
